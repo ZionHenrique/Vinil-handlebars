cda8c247b2dab4189644978ed538f65297b1bfa7
Luany Rose <lrlm@discente.ifpe.edu.br>
2025-12-02 16:10:47 -0300

    database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 const port = 3000;
+const db = require('./config/database')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
